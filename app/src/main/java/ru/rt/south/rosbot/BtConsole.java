20afd48053c76f94efcca6b9f4ffb493ccec0b85
Evgeniy.Gerasimov <slash.krd@gmail.com>
2018-11-08 01:01:13 +0400

    добавил активити консоли BT, добавил кнопки управления сервами (порты 10, 11)

---
+++ app/src/main/java/ru/rt/south/rosbot/BtConsole.java
@@ -0,0 +1,240 @@
+package ru.rt.south.rosbot;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class BtConsole extends AppCompatActivity {
+    private TextView tv;
+    private EditText et;
+
+    //BT
+    private final String DEVICE_ADDRESS="00:21:13:02:A0:88";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    byte buffer[];
+    boolean stopThread;
+    boolean deviceConnected=false;
+
+    //Servo
+    private int horServDegr = 90;
+    private int verServDegr = 90;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt_console);
+        tv = (TextView) findViewById(R.id.BTtextView);
+        et = (EditText) findViewById(R.id.BTeditText);
+    }
+
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
+                {
+                    try
+                    {
+                        int byteCount = inputStream.available();
+                        if(byteCount > 0)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes,"UTF-8");
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+                                    console(string);
+                                }
+                            });
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    public void btMain() {
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+                deviceConnected=true;
+                beginListenForData();
+                console("\nConnection Opened!\n");
+            }
+
+        }
+    }
+
+    private void console(String msg) {
+        if (tv!=null) {
+            tv.append(msg);
+        }
+    }
+
+    public void btConnect(View view) {
+        btMain();
+    }
+
+    public void btDisconnect(View view) {
+        try {
+            stopThread = true;
+            outputStream.close();
+            inputStream.close();
+            socket.close();
+            deviceConnected = false;
+            console("\nConnection Closed!\n");
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void onClickSend(View view) {
+        if (!deviceConnected) {
+            btMain();
+            return;
+        }
+
+        String string = et.getText().toString();
+        string.concat("\n");
+        et.getText().clear();
+        btCmd(string);
+        console("\nSent Data:"+string+"\n");
+
+    }
+
+    public void consoleClear(View view) {
+        tv.setText("");
+    }
+
+    public void btCmd (String cmd) {
+        if (!deviceConnected) {
+            btMain();
+            return;
+        }
+
+        try {
+            outputStream.write(cmd.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void verServPlus(View view) {
+        verServDegr += 5;
+        btCmd("sv"+Integer.toString(verServDegr)+";");
+    }
+
+    public void verServMinus(View view) {
+        verServDegr -= 5;
+        btCmd("sv"+Integer.toString(verServDegr)+";");
+    }
+
+    public void horServPlus(View view) {
+        horServDegr += 5;
+        btCmd("sh"+Integer.toString(horServDegr)+";");
+    }
+
+    public void horServMinus(View view) {
+        horServDegr -= 5;
+        btCmd("sh"+Integer.toString(horServDegr)+";");
+    }
+}
--- app/src/main/java/ru/rt/south/rosbot/MainActivity.java
+++ app/src/main/java/ru/rt/south/rosbot/MainActivity.java
@@ -1,10 +1,6 @@
 package ru.rt.south.rosbot;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements SmileFace.OnSmileySelectionListener, SmileFace.OnRatingSelectedListener {
 
@@ -27,31 +16,17 @@ public class MainActivity extends AppCompatActivity implements SmileFace.OnSmile
     private static final String TAG = "MainActivity";
     private int currentSmile;
     private TextView tv;
-    private EditText et;
-
-    //BT
-    private final String DEVICE_ADDRESS="00:21:13:02:A0:88";
-    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
-    private BluetoothDevice device;
-    private BluetoothSocket socket;
-    private OutputStream outputStream;
-    private InputStream inputStream;
-    byte buffer[];
-    boolean stopThread;
-    boolean deviceConnected=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView);
-        et = (EditText) findViewById(R.id.editText);
         mSmileFace = (SmileFace) findViewById(R.id.smileView);
         mSmileFace.setOnSmileySelectionListener(this);
         mSmileFace.setOnRatingSelectedListener(this);
-        currentSmile = SmileFace.TERRIBLE;
+        currentSmile = SmileFace.GOOD;
         mSmileFace.setSelectedSmile(currentSmile);
-        btMain();
     }
 
     @Override
@@ -65,6 +40,11 @@ public class MainActivity extends AppCompatActivity implements SmileFace.OnSmile
         int id = item.getItemId();
         if (id == R.id.menu_sql_editor) {
             Intent intent = new Intent(MainActivity.this, DatabaseActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menu_bt_console) {
+            Intent intent = new Intent(MainActivity.this, BtConsole.class);
             startActivity(intent);
             return true;
         }
@@ -107,136 +87,6 @@ public class MainActivity extends AppCompatActivity implements SmileFace.OnSmile
             currentSmile++;
         }
         mSmileFace.setSelectedSmile(currentSmile, true);
-    }
-
-    public boolean BTinit()
-    {
-        boolean found=false;
-        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
-        }
-        if(!bluetoothAdapter.isEnabled())
-        {
-            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableAdapter, 0);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
-        if(bondedDevices.isEmpty())
-        {
-            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            for (BluetoothDevice iterator : bondedDevices)
-            {
-                if(iterator.getAddress().equals(DEVICE_ADDRESS))
-                {
-                    device=iterator;
-                    found=true;
-                    break;
-                }
-            }
-        }
-        return found;
-    }
-
-    public boolean BTconnect()
-    {
-        boolean connected=true;
-        try {
-            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
-            socket.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-            connected=false;
-        }
-        if(connected)
-        {
-            try {
-                outputStream=socket.getOutputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                inputStream=socket.getInputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-
-        return connected;
-    }
-
-    void beginListenForData()
-    {
-        final Handler handler = new Handler();
-        stopThread = false;
-        buffer = new byte[1024];
-        Thread thread  = new Thread(new Runnable()
-        {
-            public void run()
-            {
-                while(!Thread.currentThread().isInterrupted() && !stopThread)
-                {
-                    try
-                    {
-                        int byteCount = inputStream.available();
-                        if(byteCount > 0)
-                        {
-                            byte[] rawBytes = new byte[byteCount];
-                            inputStream.read(rawBytes);
-                            final String string=new String(rawBytes,"UTF-8");
-                            handler.post(new Runnable() {
-                                public void run()
-                                {
-                                    tv.append(string);
-                                }
-                            });
-
-                        }
-                    }
-                    catch (IOException ex)
-                    {
-                        stopThread = true;
-                    }
-                }
-            }
-        });
-
-        thread.start();
-    }
-
-    private void btMain() {
-        if(BTinit())
-        {
-            if(BTconnect())
-            {
-                deviceConnected=true;
-                beginListenForData();
-                tv.append("\nConnection Opened!\n");
-            }
-
-        }
-    }
-
-    public void onClickSend(View view) {
-        String string = et.getText().toString();
-        string.concat("\n");
-        try {
-            outputStream.write(string.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        tv.append("\nSent Data:"+string+"\n");
-
     }
 
 }
